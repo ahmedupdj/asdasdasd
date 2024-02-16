fa05cba1ff68f545878985d2bdce7ddb4d68ec3a
ahmedupdj <67062251+ahmedupdj@users.noreply.github.com>
2024-02-17 00:48:19 +0200

    Add files via upload

---
+++ Handlers/commandHandler.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, Permissions } = require('discord.js');
+
+function loadCommands(client) {
+    const fs = require('fs');
+    const config = require('../config');
+    require('@colors/colors');
+    let commandsArray = [];
+    let developerArray = [];
+
+    const commandsFolder = fs.readdirSync('./Commands');
+    for (const folder of commandsFolder) {
+        const commandFiles = fs
+            .readdirSync(`./Commands/${folder}`)
+            .filter((file) => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const commandFile = require(`../Commands/${folder}/${file}`);
+            client.commands.set(commandFile.data.name, commandFile);
+            if (commandFile.developer) developerArray.push(commandFile.data.toJSON());
+            else commandsArray.push(commandFile.data.toJSON());
+            console.log('[Commands]'.red + ` ${file.split('.')[0]} has been loaded.`);
+            continue;
+        }
+    }
+
+    client.application.commands.set(commandsArray);
+    
+    const developerGuild = client.guilds.cache.get(config.developerGuildID);
+    if (developerGuild) {
+        developerGuild.commands.set(developerArray).then(() => {
+            console.log('Developer commands updated successfully.');
+        }).catch((error) => {
+            console.error('Error updating developer commands:', error);
+        });
+    } else {
+        console.error('Developer guild not found.');
+    }
+}
+
+module.exports = { loadCommands };
---
+++ config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    // Basic configuration
+    token: 'MTE3OTE1ODg5Mzc3NjA5NzM3MA.GLtSIN.Uo2Vus0w9Ie10BG3_jcAH889rpiC-DUCL8Ayh0', // Place here your token
+    status: 'v1.1',
+    developerGuildID: '794571384482955282', // Place here your Guild ID
+    database: 'mongodb+srv://SpecialCodes:fedswjfnwsdefklnejf@cluster1.bfiytf1.mongodb.net/?retryWrites=true&w=majority', // Place here your MongoDB access
+
+    // Ticket configuration 
+    ticketName: 'ticket-',
+    ticketDescription: '🌿 Ticket open by',
+    ticketCreate: '✅ Your ticket has been created',
+    ticketButtonCreated: 'View ticket',
+    ticketButtonCreatedEmoji: '👀',
+    ticketAlreadyExist: 'Sorry but you already have an open ticket.',
+    ticketNoPermissions: 'Sorry, but you do not have permission to do this.',
+    ticketError: 'Something went wrong, try again later.',
+    ticketMessageTitle: 'Welcome to you, thanks for opening a ticket.',
+    ticketMessageDescription: 'A member of our moderation team will soon take care of your request.\nThanks to you for waiting in calm and good mood.',
+    ticketClose: 'Close',
+    ticketCloseEmoji: '📪',
+    ticketLock: 'Lock',
+    ticketLockEmoji: '🔒',
+    ticketUnlock: 'Unlock',
+    ticketUnlockEmoji: '🔓',
+    ticketClaim: 'Claim',
+    ticketClaimEmoji: '👋',
+    ticketManage: 'Members',
+    ticketManageEmoji: '➕',
+    ticketManageMenuTitle: 'Choose a member.',
+    ticketManageMenuEmoji: '❔',
+    ticketCloseTitle: 'The ticket is currently being closed...',
+    ticketCloseDescription: 'Ticket will be closed in 5 seconds.',
+    ticketSuccessLocked: 'Ticket was locked succesfully.',
+    ticketAlreadyLocked: 'This ticket is already locked.',
+    ticketSuccessUnlocked: 'Ticket was unlocked succesfully.',
+    ticketAlreadyUnlocked: 'This ticket is already unlocked.',
+    ticketSuccessClaim: 'Ticket was successfully claimed by',
+    ticketAlreadyClaim: 'Ticket is already claimed by',
+    ticketDescriptionClaim: ', it was claimed by',
+    ticketTranscriptMember: 'Member:',
+    ticketTranscriptTicket: 'Ticket:',
+    ticketTranscriptClaimed: 'Claimed:',
+    ticketTranscriptModerator: 'Moderator:',
+    ticketTranscriptTime: 'Time:',
+    ticketMemberAdd: 'has been added to the ticket.',
+    ticketMemberRemove: 'has been removed to the ticket.',
+}
